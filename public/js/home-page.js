f319fdc67ef90699dda6912a1bacd59d6097d7ee
Mateusz <mat.kabala.91@gmail.com>
2022-02-28 21:50:40 +0100

    Add changes to GSAP animations and media query styles.

--- public/js/home-page.js
+++ public/js/home-page.js
@@ -1,5 +1,12 @@
 // Elementy charakterystyczne wyłącznie dla strony głównej.
 
+// Stałe i zmienne
+const landingPageTitle = document.querySelector('.landing-page-title');
+const landingPageSubtitle = document.querySelectorAll('.landing-page-subtitle');
+const landingPageArrow = document.querySelector('.landing-page-arrow');
+const grid = document.querySelectorAll('.grid');
+const gridBtn = document.querySelectorAll('.grid-btn');
+
 // Animacje ScrollTrigger dla landing-page
 const landingPageLeaveTl = gsap.timeline({
     scrollTrigger: {
@@ -13,45 +20,32 @@ landingPageLeaveTl.fromTo('.landing-page-container', {opacity: 1}, {opacity: 0})
 landingPageLeaveTl.fromTo('.landing-page-arrow', {opacity: 1}, {opacity: 0}, '<');
 
 // Animacje ScrollTrigger dla kolejnych sekcji grid (enter)
-const grid = document.querySelectorAll('.grid');
-const screenWidth = document.body.clientWidth;
-const enterStart = screenWidth < 1200 ? '-100%' : '-75%';
-const enterEnd = screenWidth < 1200 ? '-50%' : '-25%';
-const leaveStart = screenWidth < 1200 ? '0%' : '25%';
-const leaveEnd = screenWidth < 1200 ? '50%' : '75%';
-grid.forEach(item => {
-    const gridEnterTl = gsap.timeline({
-        scrollTrigger: {
-            trigger: item,
-            start: enterStart,
-            end: enterEnd,
-            scrub: true
-        }
-    });
-    const gridLeaveTl = gsap.timeline({
-        scrollTrigger: {
-            trigger: item,
-            start: leaveStart,
-            end: leaveEnd,
-            scrub: true
-        }
-    });
-    gsap.set(item.querySelector('.grid-text'), {opacity: 0, scale: 0.95});
-    gsap.set(item.querySelector('.grid-img'), {scale: 1.1});
-    gsap.set(item.querySelector('.grid-header'), {scale: 0.9, opacity: 0});
-    gridEnterTl.to(item.querySelector('.grid-img'), {scale: 1.2});
-    gridEnterTl.to(item.querySelector('.grid-header'), {scale: 1, opacity: 1}, '<');
-    gridEnterTl.to(item.querySelector('.grid-text'), {opacity: 1, scale: 1}, '<50%');
-
-    gridLeaveTl.to(item.querySelector('.grid-header'), {scale: 0.9, opacity: 0});
-    gridLeaveTl.to(item.querySelector('.grid-img'), {scale: 1.1}, '<');
-    gridLeaveTl.to(item.querySelector('.grid-text'), {opacity: 0, scale: 0.95}, '<50%');
-});
+if (document.body.clientWidth >= 1200) {
+    grid.forEach(item => {
+        const gridEnterTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: item,
+                start: '-100%',
+                end: '-50%',
+                scrub: true
+            }
+        });
+        const gridLeaveTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: item,
+                start: '0%',
+                end: '50%',
+                scrub: true
+            }
+        });
+        gridEnterTl.to(item.querySelector('.grid-img'), {scale: 1.2});
+        gridLeaveTl.to(item.querySelector('.grid-img'), {scale: 1.1}, '<');
+    });   
+}
 
 // Animacja przycisków 'dowiedz się więcej'
-const gridBtn = document.querySelectorAll('.grid-btn');
 gridBtn.forEach(btn => {
-    btn.addEventListener('mouseover', () => {
+    btn.addEventListener('mouseenter', () => {
         gsap.to(btn, {color: '#EDF0F0', backgroundColor: '#A47E1B', duration: 0.5});
     });
     btn.addEventListener('mouseleave', () => {
@@ -60,7 +54,6 @@ gridBtn.forEach(btn => {
 });
 
 // ScrollTo dla strzałki w dół na landing-page
-const landingPageArrow = document.querySelector('.landing-page-arrow');
 landingPageArrow.addEventListener('click', () => {
     gsap.to(window, 1, {scrollTo: {y: window.innerHeight}});
 });
@@ -74,33 +67,20 @@ landingPageArrow.addEventListener('mouseleave', () => {
 });
 
 // ScrollTo dla rozwijanego menu
-const navLinks = document.querySelectorAll('.nav-link');
-navLinks.forEach((link, index) => {
+navLink.forEach((link, index) => {
     let sectionScrolled = '.grid-' + (index + 1);
-    let sectionOffsetY = 0;
     link.addEventListener('click', (event) => {
         event.preventDefault();
-        navTl.to(window, 1, {scrollTo: {y: sectionScrolled, offsetY: sectionOffsetY}});
-        navTl.to('.nav-item', {opacity: 0, stagger: -0.05}, '<');
-        navTl.to('.exit', {scale: 0, stagger: -0.05}, '<');
-        navTl.to(nav, {height: 50}, '<50%');
-        navTl.to(nav, {width: 50});
-        navTl.to('.drop', {scale: 1}, '<');
-        nav.classList.toggle('collapsed');
+        hideMenu();
+        navTl.to(window, 1, {scrollTo: {y: sectionScrolled, offsetY: 0}});
     });
 });
 
 // Animacja przy otwieraniu strony
-const landingPageTitle = document.querySelector('.landing-page-title');
-const landingPageSubtitle = document.querySelectorAll('.landing-page-subtitle');
 const loadPageTl = gsap.timeline({defaults: {duration: 0.5, ease: 'power2.out'}});
-
 gsap.set(landingPageTitle, {opacity: 0});
 gsap.set(landingPageSubtitle, {opacity: 0});
 gsap.set(landingPageArrow, {opacity: 0});
-gsap.set(nav, {opacity: 0});
-
 loadPageTl.to(landingPageTitle, {opacity: 1, delay: 0.5});
 loadPageTl.to(landingPageSubtitle, {opacity: 1, duration: 0.5, stagger: 0.5});
 loadPageTl.to(landingPageArrow, {opacity: 1});
-loadPageTl.to(nav, {opacity: 1}, '<');
--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,26 +1,30 @@
 // Tutaj znajdują się wyłącznie skrypty dla elementów nawigacji oraz stopki, które będą powtarzalne na każdej podstronie.
 
-// Rozwijanie i zwijanie menu.
+// Stałe i zmienne
 const nav = document.querySelector('.nav');
 const navButton = document.querySelectorAll('.nav-img');
-const navTl = gsap.timeline({defaults: {duration: 0.5, ease: 'power1.out'}});
-gsap.set('.exit', {scale: 0});
+const navLink = document.querySelectorAll('.nav-link');
+const backButton = document.querySelector('.back-button');
+const contactBtn = document.querySelector('.contact-btn');
+const footerLink = document.querySelector('.footer-link');
+
+// Rozwijanie i zwijanie menu.
+const navTl = gsap.timeline({
+    defaults: {
+        duration: 0.5,
+        ease: 'power1.out'
+    }
+});
+gsap.set('.exit', {
+    scale: 0
+});
 navButton.forEach(item => {
     item.addEventListener('click', () => {
         if (nav.classList.contains('collapsed')) {
-            navTl.to('.nav-item', {opacity: 0, stagger: -0.05});
-            navTl.to('.exit', {scale: 0, stagger: -0.05}, '<');
-            navTl.to(nav, {height: 50}, '<50%');
-            navTl.to(nav, {width: 50});
-            navTl.to('.drop', {scale: 1}, '<');
+            hideMenu();
         } else {
-            navTl.to(nav, {width: 'auto'});
-            navTl.to('.drop', {scale: 0}, '<');
-            navTl.to(nav, {height: 'auto'});
-            navTl.to('.nav-item', {opacity: 1, stagger: 0.05}, '<50%');
-            navTl.to('.exit', {scale: 1}, '<');
+            showMenu();
         }
-        nav.classList.toggle('collapsed');
     });
 });
 
@@ -33,39 +37,138 @@ const backButtonTl = gsap.timeline({
         scrub: true
     }
 });
-backButtonTl.to('.back-button', {opacity: 1});
+backButtonTl.to('.back-button', {
+    opacity: 1
+});
 
 // ScrollTo dla przycisku back-button
-const backButton = document.querySelector('.back-button');
 backButton.addEventListener('click', () => {
-    gsap.to(window, 1, {scrollTo: {y: 0}});
+    gsap.to(window, 1, {
+        scrollTo: {
+            y: 0
+        }
+    });
 });
 
 // Animacje dla elementów menu (nav-link)
-const navLink = document.querySelectorAll('.nav-link');
 navLink.forEach(link => {
     link.addEventListener('mouseenter', () => {
-        gsap.fromTo(link, {color: '#EDF0F0'}, {color: '#A47E1B', ease: 'power2.out', duration: 0.5});
+        gsap.fromTo(link, {
+            color: '#EDF0F0'
+        }, {
+            color: '#A47E1B',
+            ease: 'power2.out',
+            duration: 0.5
+        });
     });
     link.addEventListener('mouseleave', () => {
-        gsap.fromTo(link, {color: '#A47E1B'}, {color: '#EDF0F0', ease: 'power2.out', duration: 0.5});
+        gsap.fromTo(link, {
+            color: '#A47E1B'
+        }, {
+            color: '#EDF0F0',
+            ease: 'power2.out',
+            duration: 0.5
+        });
     });
 });
 
 // Animacje mouseenter i mouseleave dla footer-link
-const footerLink = document.querySelector('.footer-link');
 footerLink.addEventListener('mouseenter', () => {
-    gsap.fromTo(footerLink, {color: '#EDF0F0'}, {color: '#A47E1B', ease: 'power2.out', duration: 0.5});
+    gsap.fromTo(footerLink, {
+        color: '#EDF0F0'
+    }, {
+        color: '#A47E1B',
+        ease: 'power2.out',
+        duration: 0.5
+    });
 });
 footerLink.addEventListener('mouseleave', () => {
-    gsap.fromTo(footerLink, {color: '#A47E1B'}, {color: '#EDF0F0', ease: 'power2.out', duration: 0.5});
+    gsap.fromTo(footerLink, {
+        color: '#A47E1B'
+    }, {
+        color: '#EDF0F0',
+        ease: 'power2.out',
+        duration: 0.5
+    });
 });
 
 // Animacja przycisku wysyłania formularza
-const contactBtn = document.querySelector('.contact-btn');
 contactBtn.addEventListener('mouseenter', () => {
-    gsap.fromTo(contactBtn, {backgroundColor: '#1D1D1D', color: '#A47E1B'}, {color: '#EDF0F0', backgroundColor: '#A47E1B', duration: 0.5});
+    gsap.fromTo(contactBtn, {
+        backgroundColor: '#1D1D1D',
+        color: '#A47E1B'
+    }, {
+        color: '#EDF0F0',
+        backgroundColor: '#A47E1B',
+        duration: 0.5
+    });
 });
 contactBtn.addEventListener('mouseleave', () => {
-    gsap.fromTo(contactBtn, {color: '#EDF0F0', backgroundColor: '#A47E1B'}, {backgroundColor: '#1D1D1D', color: '#A47E1B', duration: 0.5});
+    gsap.fromTo(contactBtn, {
+        color: '#EDF0F0',
+        backgroundColor: '#A47E1B'
+    }, {
+        backgroundColor: '#1D1D1D',
+        color: '#A47E1B',
+        duration: 0.5
+    });
 });
+
+
+// Funkcje
+
+function showMenu() {
+    const menuHeight = document.body.clientWidth < 768 ? window.innerHeight : 'auto';
+    navTl.to(nav, {
+        width: 'auto'
+    });
+    navTl.to('.drop', {
+        scale: 0
+    }, '<');
+    navTl.to(nav, {
+        height: menuHeight
+    });
+    navTl.to('.nav-item', {
+        opacity: 1,
+        stagger: 0.05
+    }, '<50%');
+    navTl.to('.exit', {
+        scale: 1
+    }, '<');
+    nav.classList.toggle('collapsed');
+}
+
+function hideMenu() {
+    navTl.to('.nav-item', {
+        opacity: 0,
+        stagger: -0.05
+    });
+    navTl.to('.exit', {
+        scale: 0,
+        stagger: -0.05
+    }, '<');
+    navTl.to(nav, {
+        height: 50
+    }, '<50%');
+    navTl.to(nav, {
+        width: 50
+    });
+    navTl.to('.drop', {
+        scale: 1
+    }, '<');
+    nav.classList.toggle('collapsed');
+}
+
+// Chowanie menu przy scrollowaniu w dół dla małych ekranów
+if (document.body.clientWidth < 768) {
+    let prevScrollpos = window.pageYOffset; // ile pikseli do dołu przescrollowana jest strona
+    window.onscroll = function () {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) { // sprawdza czy obecna pozycja scrolla jest mniejsza od początkowej
+            nav.style.top = "0";
+        } else {
+            nav.style.top = "-50px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+}
